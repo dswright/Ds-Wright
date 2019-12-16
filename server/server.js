7fa77513e08de653b64dc8796f3ffa00662f1c31
Dylan Wright <dylansamuelwright@gmail.com>
2019-12-16 14:00:00 -0800

    remove server console log

--- server/server.js
+++ server/server.js
@@ -3,11 +3,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const favicon = require('serve-favicon');
-
-console.log('dirname', __dirname);
-
-// app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'assets')));
 
 module.exports = app;
