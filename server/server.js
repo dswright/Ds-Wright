c224e786ee8a83186738bc8599682afb5ebb7d94
Dylan Wright <dylansamuelwright@gmail.com>
2019-12-23 12:51:23 -0800

    redirect to https

--- server/server.js
+++ server/server.js
@@ -5,4 +5,15 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'assets')));
 
+// redirect http to https
+app.use((req, res, next) => {
+  if (req.secure || req.headers.host === 'localhost:3000') {
+    // request was via https, so do no special handling
+    next();
+  } else {
+    // request was via http, so redirect to https
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+
 module.exports = app;
