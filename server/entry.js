d4d14b9935ea0720bd65ad40eabcc476257df85e
Dylan Wright <dylansamuelwright@gmail.com>
2019-12-23 12:20:57 -0800

    add hotjar tracking

--- server/entry.js
+++ server/entry.js
@@ -47,6 +47,17 @@ export default ({ clientStats }) => (req, res) => {
           <link rel="icon" href="/favicon.ico?v=1.0" type="image/x-icon" />
           <link rel="shortcut icon" href="/favicon.ico?v=1.0" />
           ${styles}
+          <!-- Hotjar Tracking Code for ds-wright.com -->
+          <script>
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:1622903,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
         </head>
         <body>
           <div id="root">${app}</div>
